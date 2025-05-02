92288d139afa083ab0148b61b54ffc58a394ca75
RASMIYA A. <arasmiya77@gmail.com>
2025-05-02 11:39:51 +0530

    Revert "Sudesh01"

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import App from './App.jsx';
+import './index.css';
+import App from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <React.StrictMode>
-    <Router>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </Router>
+    <App />
   </React.StrictMode>
 );
+
+
