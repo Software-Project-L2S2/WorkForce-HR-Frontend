c5680da4d4026444bf3a17ff083bc89562b3bb1b
vphjayawardhana <jayawardhanavph.22@uom.lk>
2025-05-07 17:11:46 +0530

    WIP: Temporary commit before switching branch

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-
-/*import './App.css';
-import {Employeeprofile} from './EmployeeProfile/Employeeprofile';
-
+// filepath: /Users/pawanhasthika/Documents/project/SoftwareProject-2ndYear/Frondend/my-react-app/src/App.js
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Employeeprofile } from './EmployeeProfile/Employeeprofile';
+import LeaveManagement from './LeaveManagement/Leavemanagement';
+import LeaveRequestForm from './EmployeeLeaveRequest/Employeeleaverequest';
 
 function App() {
   return (
-    <div>
-      <Employeeprofile/>
-   
-    </div>
+    <Router>
+      <div>
+        {/* Navigation Links */}
+        <nav>
+          <ul>
+            <li><Link to="/">Employee Profile</Link></li>
+            <li><Link to="/leave-management">Leave Management</Link></li>
+            <li><Link to="/leave-request">Leave Request Form</Link></li>
+          </ul>
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Employeeprofile />} />
+          <Route path="/leave-management" element={<LeaveManagement />} />
+          <Route path="/leave-request" element={<LeaveRequestForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-export default App;*/
-import LeaveManagement from './LeaveManagement/Leavemanagement';
-
-function App() {
-  return <LeaveManagement />;
-}
-
 export default App;
-
-/*import LeaveRequestForm from './EmployeeLeaveRequest/Employeeleaverequest';
-
-function App() {
-  return < LeaveRequestForm/>;
-}
-
-export default App;*/
